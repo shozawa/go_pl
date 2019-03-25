071bcf23f011067cc3f917adf0dd94cb4c11291d
shozawa <tomohiro.shozawa@gmail.com>
2019-03-25 09:03:34 +0900

    add color mandelbrot

---
+++ ch03/hue/hue.go
@@ -0,0 +1,24 @@
+package hue
+
+import (
+	"image/color"
+	"math"
+)
+
+const (
+	gain        = 10
+	offsetx     = 0.2
+	offsetGreen = 0.6
+)
+
+func Coloring(x float64) color.Color {
+	x = x*2 - 1
+	red := sigmoid(x, gain, -1*offsetx)
+	blue := 1 - sigmoid(x, gain, offsetx)
+	green := sigmoid(x, gain, offsetGreen) + (1 - sigmoid(x, gain, -1*offsetGreen)) - 1
+	return color.RGBA{uint8(255 * red), uint8(255 * green), uint8(255 * blue), 255}
+}
+
+func sigmoid(x, gain, offsetx float64) float64 {
+	return (math.Tanh((x+offsetx)*gain/2) + 1) / 2
+}
