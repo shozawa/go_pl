47d44bdc6505789f1bb9efe50adf176b3d9b2347
shozawa <tomohiro.shozawa@gmail.com>
2019-03-26 09:04:02 +0900

    refactor: comma

--- ch03/string/string.go
+++ ch03/string/string.go
@@ -1,9 +1,16 @@
 package string
 
+import "bytes"
+
 func comma(s string) string {
+	var buf bytes.Buffer
 	n := len(s)
-	if n <= 3 {
-		return s
+	for i, r := range s {
+		digit := n - i
+		buf.WriteRune(r)
+		if digit != 1 && digit%3 == 1 {
+			buf.WriteRune(',')
+		}
 	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+	return buf.String()
 }
