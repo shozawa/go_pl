b7229e5ee942d084be5a187f714c7541f0b2d6bd
shozawa <tomohiro.shozawa@gmail.com>
2019-04-12 08:10:31 +0900

    add "get" and "close" command

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -6,25 +6,37 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	cmd := os.Args[1]
 	switch cmd {
+	case "get":
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		issue, err := getIssue(id)
+		fmt.Print("State\tTitle\tBody\n")
+		fmt.Printf("%s\t%s\t%s\n", issue.State, issue.Title, issue.Body)
 	case "create":
 		tmpfile, _ := ioutil.TempFile("", "")
 		defer os.Remove(tmpfile.Name())
-		c := exec.Command(getEditor(), tmpfile.Name())
-		c.Stdin = os.Stdin
-		c.Stdout = os.Stdout
-		c.Stderr = os.Stderr
-		c.Run()
-		f, _ := os.Open(tmpfile.Name())
+
+		launchEditor(tmpfile.Name())
+
+		f, err := os.Open(tmpfile.Name())
+		if err != nil {
+			os.Exit(1)
+		}
 		defer f.Close()
+
 		scaner := bufio.NewScanner(f)
 		scaner.Scan()
 		title := scaner.Text()
@@ -33,9 +45,23 @@ func main() {
 			body = append(body, scaner.Text())
 		}
 		create(title, strings.Join(body, "\n"))
+	case "close":
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		closeIssue(id)
 	default:
 		fmt.Println("help here.")
 	}
+}
+
+func launchEditor(filename string) {
+	c := exec.Command(getEditor(), filename)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	c.Run()
 }
 
 func getEditor() string {
@@ -45,6 +71,57 @@ func getEditor() string {
 	} else {
 		return "vi"
 	}
+}
+
+func closeIssue(id int) {
+	token := os.Getenv("GITHUB_PERSONAL_API_TOKEN")
+	url := fmt.Sprintf("https://api.github.com/repos/shozawa/go_pl/issues/%d", id)
+
+	client := http.DefaultClient
+
+	json, _ := json.Marshal(map[string]interface{}{
+		"state": "closed",
+	})
+
+	req, err := http.NewRequest("PATCH", url, strings.NewReader(string(json)))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "token "+token)
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+
+	io.Copy(os.Stdout, res.Body)
+
+}
+
+type Issue struct {
+	Title string
+	Body  string
+	State string
+}
+
+func getIssue(id int) (*Issue, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/shozawa/go_pl/issues/%d", id)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	var issue Issue
+	if err = json.NewDecoder(res.Body).Decode(&issue); err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return &issue, nil
 }
 
 func create(title string, body string) {
