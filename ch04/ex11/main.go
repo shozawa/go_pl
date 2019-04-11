def9fb5a4fcde9a5a775aa0ab80748cfdd2ba5c9
shozawa <tomohiro.shozawa@gmail.com>
2019-04-11 21:40:42 +0900

    [update] edit issue description by $EDITOR

--- ch04/ex11/main.go
+++ ch04/ex11/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func main() {
-	create("test", "test body")
+	cmd := os.Args[1]
+	switch cmd {
+	case "create":
+		tmpfile, _ := ioutil.TempFile("", "")
+		defer os.Remove(tmpfile.Name())
+		c := exec.Command(getEditor(), tmpfile.Name())
+		c.Stdin = os.Stdin
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		c.Run()
+		f, _ := os.Open(tmpfile.Name())
+		defer f.Close()
+		scaner := bufio.NewScanner(f)
+		scaner.Scan()
+		title := scaner.Text()
+		var body []string
+		for scaner.Scan() {
+			body = append(body, scaner.Text())
+		}
+		create(title, strings.Join(body, "\n"))
+	default:
+		fmt.Println("help here.")
+	}
+}
+
+func getEditor() string {
+	editor := os.Getenv("EDITOR")
+	if len(editor) > 1 {
+		return editor
+	} else {
+		return "vi"
+	}
 }
 
 func create(title string, body string) {
