18e56c49aaf71d90ddbd44dff49c57e4d33aab81
shozawa <tomohiro.shozawa@gmail.com>
2019-04-18 07:33:49 +0900

    add ex06

---
+++ ch05/ex06/main.go
@@ -0,0 +1,30 @@
+package surface
+
+import "math"
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func Corner(i, j int) (sx, sy float64) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+	z := f(x, y)
+
+	sx = width/2 + (x-y)*cos30*xyscale
+	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
+
+	return
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+	return math.Sin(r)
+}
