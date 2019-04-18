c3a8313f7c1a538e487086debbcb5b12a770e641
shozawa <tomohiro.shozawa@gmail.com>
2019-04-19 08:37:18 +0900

    Update main.go

--- ch05/ex03/main.go
+++ ch05/ex03/main.go
@@ -7,14 +7,12 @@ import (
 	"golang.org/x/net/html"
 )
 
-// FIXME: 題意に沿ってる？
 func main() {
 	doc, err := html.Parse(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 	}
 	for _, t := range visit(nil, doc) {
-		// TODO: なんか空行がたくさん出るので削除する
 		fmt.Println(t)
 	}
 }
@@ -23,6 +21,7 @@ func visit(texts []string, n *html.Node) []string {
 	if n == nil || n.Type == html.ElementNode && (n.Data == "script" || n.Data == "style") {
 		return texts
 	}
+	// FXIME: 空のテキストノードは取り除いたほうがいいと思う
 	if n.Type == html.TextNode {
 		texts = append(texts, n.Data)
 	}
