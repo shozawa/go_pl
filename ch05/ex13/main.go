b220abf28fa5ab2633bfb9b5c6c048591bd3562e
shozawa <tomohiro.shozawa@gmail.com>
2019-04-25 08:34:18 +0900

    update ex13

--- ch05/ex13/main.go
+++ ch05/ex13/main.go
@@ -3,20 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	neturl "net/url"
 	"os"
+
 	"github.com/shozawa/go_pl/ch05/graph"
 	"github.com/shozawa/go_pl/ch05/links"
 )
 
+var urls []string = os.Args[1:]
+
 func main() {
-	graph.BFS(crawl, os.Args[1:])
+	graph.BFS(crawl, urls)
 }
 
 func crawl(url string) []string {
 	fmt.Println(url)
-	list, err := links.Exract(url)
+	list, err := links.Exract(url, isSameHost(urls, url))
 	if err != nil {
 		log.Print(err)
 	}
 	return list
+}
+
+// TODO: コレクション操作を外出しする
+func isSameHost(urls []string, url string) bool {
+	a, err := neturl.Parse(url)
+	if err != nil {
+		return false
+	}
+	for _, e := range urls {
+		b, err := neturl.Parse(e)
+		if err != nil {
+			continue
+		}
+		if a.Hostname() == b.Hostname() {
+			return true
+		}
+	}
+	return false
 }
--- ch05/links/links.go
+++ ch05/links/links.go
@@ -1,13 +1,17 @@
 package links
 
-import(
+import (
 	"fmt"
+	"io"
 	"net/http"
+	neturl "net/url"
+	"os"
+	pathpackage "path"
 
 	"golang.org/x/net/html"
 )
 
-func Exract(url string)([]string, error) {
+func Exract(url string, cache bool) ([]string, error) {
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -19,6 +23,10 @@ func Exract(url string)([]string, error) {
 	doc, err := html.Parse(res.Body)
 	if err != nil {
 		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	// ドキュメントの保存
+	if cache {
+		save(url, res)
 	}
 	var links []string
 	forEachNode(doc, func(n *html.Node) {
@@ -46,4 +54,28 @@ func forEachNode(n *html.Node, proc func(*html.Node)) {
 	proc(n)
 	forEachNode(n.FirstChild, proc)
 	forEachNode(n.NextSibling, proc)
+}
+
+func save(url string, resp *http.Response) error {
+	// FIXME: なんどもParseしてて無駄
+	u, err := neturl.Parse(url)
+	if err != nil {
+		return err
+	}
+	// FIXME: index.html 以外の名前を使う
+	dir, _ := pathpackage.Split(u.Path)
+	dir = pathpackage.Join(".", u.Hostname(), dir)
+	os.MkdirAll(dir, 0755)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(pathpackage.Join(dir, "index.html"))
+	defer f.Close()
+	// FXIME: コピーできてない...空ファイルができる
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
