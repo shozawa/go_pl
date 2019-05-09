54564c59f387da5c377dc6e321c358fa6d278055
shozawa <tomohiro.shozawa@gmail.com>
2019-05-09 20:33:06 +0900

    refactor

--- ch06/intset/intset.go
+++ ch06/intset/intset.go
@@ -84,11 +84,7 @@ func (s *IntSet) UnionWith(other *IntSet) *IntSet {
 
 func (s *IntSet) IntersectWith(other *IntSet) *IntSet {
 	result := &IntSet{}
-	// FIXME: もっときれいに
-	for i := range s.words {
-		if len(other.words) <= i {
-			break
-		}
+	for i := 0; i < min(len(s.words), len(other.words)); i++ {
 		word := s.words[i] & other.words[i]
 		result.words = append(result.words, word)
 	}
