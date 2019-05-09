6c9a77127c65890732e8d3eb058efa5f0f6fe78f
shozawa <tomohiro.shozawa@gmail.com>
2019-05-09 21:03:19 +0900

    refactor test

--- ch06/intset/intset_test.go
+++ ch06/intset/intset_test.go
@@ -36,19 +36,15 @@ func TestElems(t *testing.T) {
 
 func TestString(t *testing.T) {
 	tests := []struct {
-		numbers []int
-		want    string
+		set  *IntSet
+		want string
 	}{
-		{[]int{}, "{}"},
-		{[]int{100}, "{100}"},
-		{[]int{1, 2, 3}, "{1 2 3}"},
+		{New(), "{}"},
+		{New(100), "{100}"},
+		{New(1, 2, 3), "{1 2 3}"},
 	}
 	for _, test := range tests {
-		set := &IntSet{}
-		for _, n := range test.numbers {
-			set.Add(n)
-		}
-		if got := set.String(); got != test.want {
+		if got := test.set.String(); got != test.want {
 			t.Errorf("set.String() is not %q. got=%q", test.want, got)
 		}
 	}
@@ -56,50 +52,38 @@ func TestString(t *testing.T) {
 
 func TestHas(t *testing.T) {
 	tests := []struct {
-		numbers []int
-		arg     int
-		want    bool
+		set  *IntSet
+		arg  int
+		want bool
 	}{
-		{[]int{}, 1, false},
-		{[]int{1}, 1, true},
-		{[]int{1, 2}, 1, true},
-		{[]int{65, 66}, 66, true},
-		{[]int{1, 2}, 66, false},
+		{New(), 1, false},
+		{New(1), 1, true},
+		{New(1, 2), 1, true},
+		{New(65, 66), 66, true},
+		{New(1, 2), 66, false},
 	}
 	for _, test := range tests {
-		set := &IntSet{}
-		for _, n := range test.numbers {
-			set.Add(n)
-		}
-		if got := set.Has(test.arg); got != test.want {
-			t.Errorf("set.Has(%d) %s is not %v. got=%v", test.arg, set.String(), test.want, got)
+		if got := test.set.Has(test.arg); got != test.want {
+			t.Errorf("set.Has(%d) %s is not %v. got=%v", test.arg, test.set.String(), test.want, got)
 		}
 	}
 }
 
 func TestUnionWith(t *testing.T) {
 	tests := []struct {
-		a    []int
-		b    []int
+		a    *IntSet
+		b    *IntSet
 		want string
 	}{
-		{[]int{}, []int{}, "{}"},
-		{[]int{1}, []int{2}, "{1 2}"},
-		{[]int{1, 2}, []int{3}, "{1 2 3}"},
-		{[]int{}, []int{200}, "{200}"},
-		{[]int{1, 100}, []int{100, 200}, "{1 100 200}"},
+		{New(), New(), "{}"},
+		{New(1), New(2), "{1 2}"},
+		{New(1, 2), New(3), "{1 2 3}"},
+		{New(), New(200), "{200}"},
+		{New(1, 100), New(100, 200), "{1 100 200}"},
 	}
 	for _, test := range tests {
-		a := &IntSet{}
-		for _, n := range test.a {
-			a.Add(n)
-		}
-		b := &IntSet{}
-		for _, n := range test.b {
-			b.Add(n)
-		}
-		a = a.UnionWith(b)
-		if got := a.String(); got != test.want {
+		union := test.a.UnionWith(test.b)
+		if got := union.String(); got != test.want {
 			t.Errorf("union is not %s. got=%s", test.want, got)
 		}
 	}
@@ -167,45 +151,37 @@ func TestSymetricDifferenceWith(t *testing.T) {
 
 func TestLen(t *testing.T) {
 	tests := []struct {
-		numbers []int
-		want    int
+		set  *IntSet
+		want int
 	}{
-		{[]int{}, 0},
-		{[]int{1}, 1},
-		{[]int{1, 2}, 2},
-		{[]int{65, 66}, 2},
-		{[]int{1, 2, 65, 128}, 4},
+		{New(), 0},
+		{New(1), 1},
+		{New(1, 2), 2},
+		{New(65, 66), 2},
+		{New(1, 2, 65, 128), 4},
 	}
 	for _, test := range tests {
-		set := &IntSet{}
-		for _, n := range test.numbers {
-			set.Add(n)
-		}
-		if got := set.Len(); got != test.want {
-			t.Errorf("set.Len() %s is not %v. got=%v", set.String(), test.want, got)
+		if got := test.set.Len(); got != test.want {
+			t.Errorf("set.Len() %s is not %v. got=%v", test.set.String(), test.want, got)
 		}
 	}
 }
 
 func TestRemove(t *testing.T) {
 	tests := []struct {
-		numbers []int
+		set     *IntSet
 		removed int
 		want    string
 	}{
-		{[]int{1}, 1, "{}"},
-		{[]int{1, 2, 3}, 2, "{1 3}"},
-		{[]int{1, 2, 3}, 5, "{1 2 3}"},
-		{[]int{1, 65, 66}, 65, "{1 66}"},
+		{New(1), 1, "{}"},
+		{New(1, 2, 3), 2, "{1 3}"},
+		{New(1, 2, 3), 5, "{1 2 3}"},
+		{New(1, 65, 66), 65, "{1 66}"},
 	}
 	for _, test := range tests {
-		set := &IntSet{}
-		for _, n := range test.numbers {
-			set.Add(n)
-		}
-		set.Remove(test.removed)
-		if got := set.String(); got != test.want {
-			t.Errorf("set.Remove(%d) %s is not %q. got=%q", test.removed, set.String(), test.want, got)
+		test.set.Remove(test.removed)
+		if got := test.set.String(); got != test.want {
+			t.Errorf("set.Remove(%d) %s is not %q. got=%q", test.removed, test.set.String(), test.want, got)
 		}
 	}
 }
