85ca418970ccdb9d649b352eb83878644c3a1217
shozawa <tomohiro.shozawa@gmail.com>
2019-05-21 08:09:57 +0900

    refactor: use Mux

--- ch08/http/main.go
+++ ch08/http/main.go
@@ -13,10 +13,12 @@ func (d dollers) String() string { return fmt.Sprintf("$%.2f", d) }
 
 func main() {
 	db := database{"shoes": 50, "socks": 5}
-	log.Fatal(http.ListenAndServe("localhost:8000", db))
+	mux := http.NewServeMux()
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
-func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (db database) list(w http.ResponseWriter, req *http.Request) {
 	for item, price := range db {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
