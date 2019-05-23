dfa174f4c17e3debc5b7a6074ebc16b9c2c88d76
shozawa <tomohiro.shozawa@gmail.com>
2019-05-24 08:31:31 +0900

    refactor

--- ch08/evaluator/eval.go
+++ ch08/evaluator/eval.go
@@ -1,10 +1,75 @@
 package evaluator
 
-import "github.com/shozawa/go_pl/ch08/lexer"
+import (
+	"fmt"
+	"math"
+
+	"github.com/shozawa/go_pl/ch08/lexer"
+)
 
 func Eval(input string, env Env) float64 {
 	l := lexer.New(input)
 	p := NewParser(l)
 	program := p.Parse()
 	return program.Eval(env)
+}
+
+func (p program) Eval(env Env) float64 {
+	var result float64
+	for _, stmt := range p.statements {
+		result = stmt.Eval(env)
+	}
+	return result
+}
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
+func (l literal) Eval(_ Env) float64 {
+	return float64(l)
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	case "min":
+		left := c.args[0].Eval(env)
+		right := c.args[1].Eval(env)
+		if left < right {
+			return left
+		} else {
+			return right
+		}
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
