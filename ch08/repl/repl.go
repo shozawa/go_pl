ee4afe212bd376d8e3a19b4f5501eecf9797175f
shozawa <tomohiro.shozawa@gmail.com>
2019-05-23 20:23:30 +0900

    add REPL

---
+++ ch08/repl/repl.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/shozawa/go_pl/ch08/evaluator"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	env := make(evaluator.Env)
+	for {
+		fmt.Printf(">> ")
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		fmt.Println(evaluator.Eval(scanner.Text(), env))
+	}
+}
