9f47403f2a32f7199ce5e9c71ca2df8f65328a91
shozawa <tomohiro.shozawa@gmail.com>
2019-05-29 07:49:52 +0900

    add pipeline1

---
+++ ch08/pipeline1/pipeline1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	// Squarer
+	go func() {
+		for {
+			x := <-naturals
+			squares <- x * x
+		}
+	}()
+
+	for {
+		time.Sleep(1 * time.Second)
+		fmt.Println(<-squares)
+	}
+}
