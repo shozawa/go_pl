9f2d64637c378b08b66b7b2ac438ef2817e43020
shozawa <tomohiro.shozawa@gmail.com>
2019-06-04 08:10:34 +0900

    use select to countdown.

--- ch08/countdown1/countdown1.go
+++ ch08/countdown1/countdown1.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
-	fmt.Println("Commencing countdown.")
-	tick := time.Tick(1 * time.Second)
-	for countdown := 10; countdown > 0; countdown-- {
-		fmt.Println(countdown)
-		<-tick
+	abort := make(chan struct{})
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		abort <- struct{}{}
+	}()
+
+	fmt.Println("Commencing countdown. Press return to abort.")
+	select {
+	case <-time.After(10 * time.Second):
+	case <-abort:
+		fmt.Println("Launch aborted!")
+		return
 	}
 	launch()
 }
